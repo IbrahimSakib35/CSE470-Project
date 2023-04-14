5351e0c12b8553a7859546c63da1b017253332e1
IbrahimSakib35 <128493670+IbrahimSakib35@users.noreply.github.com>
2023-04-14 20:33:12 +0000

    commit

--- Codes/login_register.php
+++ Codes/login_register.php
@@ -1,2 +1,59 @@
 <?php
+require('connection.php');
+if(isset($_POST['register']))
+{
+    $user_exist_query="SELECT * FROM 'user' WHERE 'username'='$_POST[username]' OR 'email'='$_POST[email]'";
+    $result=mysqli_query($con,$user_exist_query);
+    if($result)
+    {
+        if(mysqli_num_rows($result)>0)
+        {
+            $result_fetch=mysqli_fetch_assoc($result);
+            if($result_fetch['username']==$_POST['username'])
+            {
+                echo"<script>
+                alert('$result_fetch[username]- This username already taken');
+                window.location.href='index.php';
+                </script>";
+            }
+            else
+            {
+                echo"<script>
+                alert('$result_fetch[email]- This email already taken');
+                window.location.href='index.php';
+                </script>";
+            }
+
+        }
+        else
+        {
+            $query="INSERT INTO `user`(`fifa_username`, `username`, `email`, `password`) VALUES ('$_POST[fifaname]','$_POST[username]','$_POST[email]','$_POST[password]')";
+            if(mysqli_query($con,$query))
+            {
+                echo"<script>
+                alert('Registration Successful! Login back');
+                window.location.href='index.php';
+                </script>";
+
+            }
+            else
+            {
+                echo"<script>
+                alert('Cannot Run Query');
+                window.location.href='index.php';
+                </script>";
+
+            }
+
+        }
+
+    }
+    else
+    {
+        echo"<script>
+        alert('Cannot Run Query');
+        window.location.href='index.php';
+        </script>";
+    }
+}
 ?>
